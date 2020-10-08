52eddccc913ef04d51b82f8a9473d3876ca2942f
ValentinAvit <55392029+ValentinAvit@users.noreply.github.com>
2020-10-08 18:30:16 +0200

    Update summary.R

--- Audrey/App/summary.R
+++ Audrey/App/summary.R
@@ -3,7 +3,8 @@
 # Import Dataset ----
 require(data.table)
 library(readr)
-data_tot <- read_csv("E:/Cours/M2_Stat/UFOs/App/scrubbed.csv", col_types = cols(comments = col_skip(),`date posted` = col_skip(), datetime = col_skip(),
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Répertoire de travail = répertoire du fichier R (où on doit mettre les data, du coup)
+data_tot <- read_csv("scrubbed.csv", col_types = cols(comments = col_skip(),`date posted` = col_skip(), datetime = col_skip(),
                                                                                 `duration (hours/min)` = col_skip(),
                                                                                 `duration (seconds)` = col_skip(), shape = col_character(),
                                                                                 state = col_skip()), locale = locale())
@@ -12,23 +13,16 @@ data_na <- as.data.table(data_na)
 data_hist <- data_na[, list(n = .N), by = shape]
 
 data_hist
-#On enlève les donnees manquante
+#On enl?ve les donnees manquante
 data_hist <- data_hist[-15,]
-
 #On rassemble circle et disk
 data_hist$n[data_hist$shape == "circle"] <- data_hist$n[data_hist$shape == "circle"] + data_hist$n[data_hist$shape == "disk"]
 data_hist <- data_hist[-5,]
-
 # On rassemble changing et changed
-data_hist <- data_hist[-26,]
+data_hist <- data_hist[-28,]
 data_hist$n[data_hist$shape == "changing"] <- data_hist$n[data_hist$shape == "changing"] + 1
 
-#On rassemble other et unknown
-data_hist$n[data_hist$shape == "unknown"] <- data_hist$n[data_hist$shape == "unknown"] + data_hist$n[data_hist$shape == "other"]
-data_hist <- data_hist[-8,]
-
 #Order
-data_hist_ord <- data_hist[order(n, decreasing = T)]
-data_hist_ord$shape = with(data_hist_ord, reorder(shape, n, mean))
-
+data_hist <- data_hist[order(n, decreasing = T)]
+data_hist$shape = with(data_hist, reorder(shape, n, mean))
 
