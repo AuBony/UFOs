f2bb55da5c3d47fafb1057b55b639becf9808377
ValentinAvit <55392029+ValentinAvit@users.noreply.github.com>
2020-10-08 18:30:50 +0200

    Update carte.R

--- Audrey/App/carte.R
+++ Audrey/App/carte.R
@@ -5,7 +5,8 @@ require(data.table)
 
 # Import Dataset ----
 library(readr)
-data_tot <- read_csv("E:/Cours/M2_Stat/UFOs/App/scrubbed.csv", col_types = cols(comments = col_skip(),`date posted` = col_skip(), datetime = col_skip(),
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Répertoire de travail = répertoire du fichier R (où on doit mettre les data, du coup)
+data_tot <- read_csv("scrubbed.csv", col_types = cols(comments = col_skip(),`date posted` = col_skip(), datetime = col_skip(),
                                                                           `duration (hours/min)` = col_skip(),
                                                                           `duration (seconds)` = col_skip(), shape = col_character(),
                                                                           state = col_skip()), locale = locale())
@@ -15,3 +16,4 @@ data_tot <- as.data.table(data_tot)
 
 data_us <- data_tot[country == "us"] #selection des data us
 data_xy <- unique(data_us[, sighting := .N , by = list(latitude, longitude)]) # agregation du nombre de vue aux us
+
