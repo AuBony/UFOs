060ca210d9c540bf95964ed00e1b91b77074c1b8
AuBony <55869341+AuBony@users.noreply.github.com>
2020-10-08 18:15:01 +0200

    Update summary.R

--- Audrey/App/summary.R
+++ Audrey/App/summary.R
@@ -12,16 +12,23 @@ data_na <- as.data.table(data_na)
 data_hist <- data_na[, list(n = .N), by = shape]
 
 data_hist
-#On enlève les donnees manquante
+#On enlÃ¨ve les donnees manquante
 data_hist <- data_hist[-15,]
+
 #On rassemble circle et disk
 data_hist$n[data_hist$shape == "circle"] <- data_hist$n[data_hist$shape == "circle"] + data_hist$n[data_hist$shape == "disk"]
 data_hist <- data_hist[-5,]
+
 # On rassemble changing et changed
-data_hist <- data_hist[-28,]
+data_hist <- data_hist[-26,]
 data_hist$n[data_hist$shape == "changing"] <- data_hist$n[data_hist$shape == "changing"] + 1
 
+#On rassemble other et unknown
+data_hist$n[data_hist$shape == "unknown"] <- data_hist$n[data_hist$shape == "unknown"] + data_hist$n[data_hist$shape == "other"]
+data_hist <- data_hist[-8,]
+
 #Order
-data_hist <- data_hist[order(n, decreasing = T)]
-data_hist$shape = with(data_hist, reorder(shape, n, mean))
+data_hist_ord <- data_hist[order(n, decreasing = T)]
+data_hist_ord$shape = with(data_hist_ord, reorder(shape, n, mean))
+
 
