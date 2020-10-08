3351f019445470c594d9da31b517b687c1556295
ValentinAvit <55392029+ValentinAvit@users.noreply.github.com>
2020-10-08 18:29:34 +0200

    Update ui.r

--- Audrey/App/ui.r
+++ Audrey/App/ui.r
@@ -7,16 +7,68 @@
 #    http://shiny.rstudio.com/
 #
 
-#anatextu : Analyse Textuelle
-
+#Package
 library(shiny)
 library(colourpicker)
 library(leaflet)
 library(ggplot2)
+library(shinythemes)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Répertoire de travail = répertoire du fichier R (où on doit mettre les data, du coup)
+
+
+source("analyse_textuelle.r")
+source("carte.r")
+source("summary.R")
+
+
 
 # Define UI for application that draws a histogram
 shinyUI(
-  navbarPage("Observations d'OVNIs",
+  navbarPage("Observations d'OVNIs", theme = shinytheme(theme = "darkly"),
+             
+             
+             
+             tabPanel("Accueil",
+                      div("Test", align = "center", size="200%"),
+                      tags$q("Test", style=("font-size: 200%; color: #FFFFFF; ")  )
+             ),
+             
+             
+             
+             #Survol des Données
+             navbarMenu("Survol des données",
+                        tabPanel("Résumé du jeu de données"
+
+
+                        ),
+
+                        tabPanel("Etude des formes",
+                                 fluidRow(
+                                   column(width = 12/3,
+                                          plotOutput("hist_forme"))
+                                 )
+                        ),
+                        tabPanel("Truc2",
+                                 sidebarLayout(
+                                   sidebarPanel(
+                                     textInput("Truc", "truc", value="Tructruc"),
+                                     br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
+                                     br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
+                                     width=2
+                                   ),
+                                   mainPanel(
+                                     div("plopidou"),
+                                     width=10
+                                   )
+                                 )
+                        ),
+                        tabPanel("Truc3"
+
+                        )
+             ),
+             
+             
              
              #Analyse Textuelle
              tabPanel("Analyse textuelle",
@@ -28,28 +80,31 @@ shinyUI(
                                              min = 2,
                                              max = 10,
                                              value=5
-                                             )
+                                 )
                                )
                         ),
                         column(width=10,
-                           #   tabPanel("Carte de mots",
-                                       plotOutput("anatextu.carte")
-                            #  )
+                               #   tabPanel("Carte de mots",
+                               plotOutput("anatextu.carte", height="1000px")
+                               #  )
                         )
                       )
              ),
              
+             
+             
              #Carte
              tabPanel("Carte du monde",
                       fluidRow(
-                        column(width=6,
+                        column(width=3,
                                wellPanel(
                                  sliderInput("carte.min.sighting",
                                              "Minimum d'observations dans la ville :",
                                              min = 10,
                                              max = 300,
                                              value=100
-                                 ))
+                                 )
+                               )
                         ),
                         column(width=9,
                                leafletOutput("carte.carte")
@@ -58,52 +113,17 @@ shinyUI(
                       )
              ),
              
-             #Summary
-             tabPanel("Summary",
-                      
-                      fluidRow(
-                        
-                        column(width = 12/2,
-                               plotOutput("hist_forme")
-                        ),
-                        
-                        column(width = 12/4
-                        ),
-                        
-                        column(width = 12/4, 
-                               checkboxGroupInput("hist_checkGroup", 
-                                                  h3("Shape selection"), 
-                                                  choices = list("changing" = 1,
-                                                                 "chevron" = 2,
-                                                                 "cigar" = 3,
-                                                                 "circle" = 4,
-                                                                 "crescent" = 5,
-                                                                 "cross" = 6,
-                                                                 "cone" = 7,
-                                                                 "cylinder" = 8,
-                                                                 "diamond" = 9,
-                                                                 "egg"= 10, 
-                                                                 "fireball" = 11,
-                                                                 "flare" = 12,
-                                                                 "flash" = 13,
-                                                                 "formation" = 14,
-                                                                 "hexagon" = 15,
-                                                                 "light" = 16,
-                                                                 "oval" = 17,
-                                                                 "pyramid" = 18,
-                                                                 "rectangle" = 19,
-                                                                 "round" = 20,
-                                                                 "sphere" = 21,
-                                                                 "teardrop" = 22,
-                                                                 "triangle" = 23,
-                                                                 "unknown" = 24
-                                                  ),
-                                                  selected = c(4, 11, 16, 23)
-                                )
-                        )
-                        
-                      )
-            )
+             
+             
+             tabPanel("Etude temporelle des données"
+             ),
+             
+             
+             
+             tabPanel("AFM"
+             )
+             
+             
+             
   )
 )
-
