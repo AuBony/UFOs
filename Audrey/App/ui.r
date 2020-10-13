388fc10ab4b397c8103dc74cefd854d676b8bc75
AuBony <audrey.bony@yahoo.fr>
2020-10-13 23:46:29 +0200

    Update ui.r

--- Audrey/App/ui.r
+++ Audrey/App/ui.r
@@ -194,7 +194,7 @@ shinyUI(
           ),
 
 
-
+          #Credit
           tabPanel("Credit",
                    fluidPage(
                      HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/pSb7PxPbi6k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
